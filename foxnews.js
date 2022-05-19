d19c3ca2fd0ac2bfe1ffce839ba47df5f70cb577
Nikita <norenkonikita@gmail.com>
2022-05-19 19:56:49 +0300

    able to write to json

--- foxnews.js
+++ foxnews.js
@@ -1,34 +1,88 @@
-// const { data } = require('cheerio/lib/api/attributes')
-import fetch from 'node-fetch'
-import * as cheerio from 'cheerio'
-import fs from 'fs'
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+import pretty from 'pretty';
+import fs, { writeFile } from 'fs';
+import replace from 'replace-in-file';
 
-const getData = (url) => {
-    return fetch(url)
-        .then(res => res.text())
-        .then(data => {
-            return data
-        })
-        .catch(err => {
-            console.log(err)
-        })
+let news = []
+
+async function getNewsUrls() {
+    try {
+        const { data } = await axios.get('https://www.foxnews.com/us');
+        const $ = cheerio.load(data);
+        const linksToNews = $('.collection.collection-article-list > div.content.article-list > article.article > div.m > a');
+        const newsUrls = [];
+
+        linksToNews.each((i, link) => {
+            newsUrls.push($(link).attr('href'));
+        });
+        return newsUrls;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
-const URL = 'https://www.foxnews.com/us'
+const options = {
+    files: 'foxnews.json'
+}
+
+function building(news, title, subTitle, content, callback) {
+    if (title.length > 0 && subTitle.length > 0) {
+
+        return news.push({
+            title,
+            subTitle,
+            content
+        })
+    }
+    callback(news)
+}
+
+function write(data) {
+    fs.writeFile('foxnews.json', JSON.stringify(data, null, 2), function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Saved!');
+        }
+    })
+}
+
+
+// fs.writeFile('foxnews.json', JSON.stringify({title, subTitle, content}, null, 2), (err, data) => err ? console.log(err) : console.log('saved'))
+
 
 const getNews = async () => {
-    const getNewsData = await getData(URL)
 
-    const newsData = cheerio.load(getNewsData)
-    const news = newsData('div.content.article-list')
-    console.log(news.html());
+    const urlsBase = await getNewsUrls()
+    let urls = []
+    urlsBase.forEach(el => {
+        if (el[0] === '/') {
+            urls.push('https://www.foxnews.com' + el);
+        } else {
+            urls.push(el)
+        }
+    })
+
+    urls.forEach(async url => {
+        try {
+            const { data } = await axios.get(url);
+            const $ = cheerio.load(data);
+            const title = $('.headline').text()
+            const subTitle = $('.sub-headline').text()
+            const content = $('.article-body').html()
+            const html = content != null ? content.replace(/"/g, '\'') : ''
+
+            building(news, title, subTitle, html, write)
 
 
-    fs.appendFile('foxnews.html', news.html(), function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
+        } catch (error) {
+            console.error(error)
+        }
+    })
 }
 
 
 getNews()
+
+
