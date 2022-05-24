bc6a4cabda10638681d5d12ac0b26902d4c36a3d
Nikita <norenkonikita@gmail.com>
2022-05-24 17:22:39 +0300

    correct working scraper

--- components/changeUrls.js
+++ components/changeUrls.js
@@ -1,10 +1,12 @@
 const changeUrls = (urls) => {
     const urlsBase = urls;
     let changedUrls = [];
-
+    
     urlsBase.forEach((el) => {
         if (el[0] === '/') {
             changedUrls.push('https://www.foxnews.com' + el);
+        } else if(el.includes('video')) {
+            return null
         } else {
             changedUrls.push(el);
         }
--- getNews.js
+++ getNews.js
@@ -6,6 +6,8 @@ import { fileURLToPath } from 'url';
 import getNewsUrls from './components/getUrls.js';
 import changeUrls from './components/changeUrls.js';
 import toXML from 'jstoxml';
+
+// create and connect to database
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const file = join(__dirname, './dataBase/db.json');
 const adapter = new JSONFile(file);
@@ -23,53 +25,57 @@ let gettingNews = new Promise((resolve, reject) => {
         return changeUrls(data);
     })
     .then(async (data) => {
-        // db.data = { news: [] };
-        let { news  } = db.data;
+        let newNews = [];
 
+        for (const item of data) {
+            const { data } = await axios.get(item);
+            const $ = cheerio.load(data);
+            const titleNews = $('.headline').text();
+            const subTitle = $('.sub-headline').text();
+            // const image = $('.m');
 
-        data.forEach(async (url) => {
-                try {
-                    const { data } = await axios.get(url);
-                    const $ = cheerio.load(data);
-                    const titleNews = $('.headline').text();
-                    const subTitle = $('.sub-headline').text();
-                    const image = $('.m');
+            $('.control').remove();
+            $('.featured-video').remove();
+            $('.speechkit-wrapper').remove();
+            $('.ad-container').remove();
+            $('.contain').remove();
+            $('p:contains("CLICK HERE TO GET THE FOX NEWS APP")').remove();
 
-                    const toRemove = (selector) => $(selector).remove();
-                    toRemove('.control');
-                    toRemove('.featured-video');
-                    toRemove('.speechkit-wrapper');
-                    toRemove('.ad-container');
-                    toRemove('.contain');
-                    $('p:contains("CLICK HERE TO GET THE FOX NEWS APP")').remove();
+            const content = $('.article-body').html();
+            const html = content != null ? content.replace(/"/g, "'") : '';
 
-                    const content = $('.article-body').html();
+            const formatHtml = (html) => {
+                let data = html.replace(
+                    /class='featured featured-video video-ct'|data-v-13907676=''|/g,
+                    ''
+                );
+                return data;
+            };
 
-                    const html = content != null ? content.replace(/"/g, "'") : '';
+            if (titleNews.length > 0 && subTitle.length > 0) {
+                newNews.push({
+                    title: titleNews,
+                    subTitle: subTitle,
+                    content: formatHtml(html),
+                });
+            }
+        }
 
-                    const formatHtml = (html) => {
-                        let data = html.replace(
-                            /class='featured featured-video video-ct'|data-v-13907676=''|/g,
-                            ''
-                        );
-                        return data;
-                    };
+        return newNews;
+    })
+    .then(async (data) => {
+        let { news } = db.data;
 
-                    if (titleNews.length > 0 && subTitle.length > 0) {
-                        news.push({
-                            title: titleNews,
-                            subTitle: subTitle,
-                            content: formatHtml(html),
-                        });
-                        db.write();
-                    }
-
-
-                } catch (error) {
-                    console.error(error);
+        if (news.length <= 0) {
+            news.push(...data);
+            await db.write();
+        } else {
+            data.forEach((item) => {
+                if (!news.find((news) => news.title === item.title)) {
+                    news.push(item);
                 }
             });
-
+            await db.write();
+        }
     })
-    // .then((data) => console.log(data))
     .then(() => console.log('done'));
