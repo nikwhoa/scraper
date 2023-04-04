7384506698f506f8e47878f5aa3b4c256327735d
Nikita <norenkonikita@gmail.com>
2023-04-04 14:01:50 +0300

    cryptonews output

--- components/output.js
+++ components/output.js
@@ -3,113 +3,132 @@ import { Low, JSONFile } from 'lowdb';
 import fs from 'fs';
 import connectDatabase from '../connectDatabase.js';
 
-const washingtonpost = connectDatabase('washingtonpost.json').then(async (data) => {
+const cryptonews = connectDatabase('cryptonews.json').then(async (data) => {
+  const adaptercryptonews = new JSONFile(data);
+  const cryptonewsDB = new Low(adaptercryptonews);
+  await cryptonewsDB.read();
+  const { item } = cryptonewsDB.data;
+
+  const output = item.map((item) => {
+    return `${item.title} <br><b>${item.pubDate}</b>`;
+  });
+
+  return output;
+});
+
+const washingtonpost = connectDatabase('washingtonpost.json').then(
+  async (data) => {
     const adapterwashingtonpost = new JSONFile(data);
     const washingtonpostDB = new Low(adapterwashingtonpost);
     await washingtonpostDB.read();
     const { item } = washingtonpostDB.data;
 
     const output = item.map((item) => {
-        return `${item.title} <br><b>${item.pubDate}</b>`;
+      return `${item.title} <br><b>${item.pubDate}</b>`;
     });
 
     return output;
-});
+  },
+);
 
-const usaTodaySports = connectDatabase('usatodaySport.json').then(async (data) => {
+const usaTodaySports = connectDatabase('usatodaySport.json').then(
+  async (data) => {
     const adapterusaTodaySports = new JSONFile(data);
     const usaTodaySportsDB = new Low(adapterusaTodaySports);
     await usaTodaySportsDB.read();
     const { item } = usaTodaySportsDB.data;
 
     const output = item.map((item) => {
-        return `${item.title} <br><b>${item.pubDate}</b>`;
+      return `${item.title} <br><b>${item.pubDate}</b>`;
     });
 
     return output;
-});
+  },
+);
 
 const cnnWorld = connectDatabase('cnnWorld.json').then(async (data) => {
-    const adaptercnnWorld = new JSONFile(data);
-    const cnnWorldDB = new Low(adaptercnnWorld);
-    await cnnWorldDB.read();
-    const { item } = cnnWorldDB.data;
+  const adaptercnnWorld = new JSONFile(data);
+  const cnnWorldDB = new Low(adaptercnnWorld);
+  await cnnWorldDB.read();
+  const { item } = cnnWorldDB.data;
 
-    const output = item.map((item) => {
-        return item.title + ' ' + '<br><b>' + item.pubDate + '</b>';
-    });
+  const output = item.map((item) => {
+    return item.title + ' ' + '<br><b>' + item.pubDate + '</b>';
+  });
 
-    return output;
+  return output;
 });
 
 const cnbcEconomy = connectDatabase('cnbcEconomy.json').then(async (data) => {
-    const adaptercnbcEconomy = new JSONFile(data);
-    const cnbcEconomyDB = new Low(adaptercnbcEconomy);
-    await cnbcEconomyDB.read();
-    const { item } = cnbcEconomyDB.data;
+  const adaptercnbcEconomy = new JSONFile(data);
+  const cnbcEconomyDB = new Low(adaptercnbcEconomy);
+  await cnbcEconomyDB.read();
+  const { item } = cnbcEconomyDB.data;
 
-    const output = item.map((item) => {
-        return item.title + ' ' + '<br><b>' + item.pubDate + '</b>';
-    });
+  const output = item.map((item) => {
+    return item.title + ' ' + '<br><b>' + item.pubDate + '</b>';
+  });
 
-    return output;
+  return output;
 });
 
 const investingNews = connectDatabase('investingNews.json').then(
-    async (data) => {
-        const adapterinvestingNews = new JSONFile(data);
-        const investingNewsDB = new Low(adapterinvestingNews);
-        await investingNewsDB.read();
-        const { item } = investingNewsDB.data;
+  async (data) => {
+    const adapterinvestingNews = new JSONFile(data);
+    const investingNewsDB = new Low(adapterinvestingNews);
+    await investingNewsDB.read();
+    const { item } = investingNewsDB.data;
 
-        const output = item.map((item) => {
-            return item.title + ' ' + '<br><b>' + item.pubDate + '</b>';
-        });
+    const output = item.map((item) => {
+      return item.title + ' ' + '<br><b>' + item.pubDate + '</b>';
+    });
 
-        return output;
-    },
+    return output;
+  },
 );
 
-const foolInvestingNews  = connectDatabase('foolInvestingNews.json').then(
-    async (data) => {
-        const adapterfoolInvestingNews = new JSONFile(data);
-        const foolInvestingNewsDB = new Low(adapterfoolInvestingNews);
-        await foolInvestingNewsDB.read();
-        const { item } = foolInvestingNewsDB.data;
+const foolInvestingNews = connectDatabase('foolInvestingNews.json').then(
+  async (data) => {
+    const adapterfoolInvestingNews = new JSONFile(data);
+    const foolInvestingNewsDB = new Low(adapterfoolInvestingNews);
+    await foolInvestingNewsDB.read();
+    const { item } = foolInvestingNewsDB.data;
 
-        const output = item.map((item) => {
-            return item.title + ' ' + '<br><b>' + item.pubDate + '</b>';
-        });
+    const output = item.map((item) => {
+      return item.title + ' ' + '<br><b>' + item.pubDate + '</b>';
+    });
 
-        return output;
-    },
+    return output;
+  },
 );
 
 const writeOutput = async () => {
-    const washingtonpostResult = await washingtonpost;
-    const usaTodaySportsResult = await usaTodaySports;
-    const cnnWorldResult = await cnnWorld;
-    const cnbcEconomyResult = await cnbcEconomy;
-    const investingNewsResult = await investingNews;
-    const foolInvestingNewsResult = await foolInvestingNews;
+  const cryptonewsResult = await cryptonews;
+  const washingtonpostResult = await washingtonpost;
+  const usaTodaySportsResult = await usaTodaySports;
+  const cnnWorldResult = await cnnWorld;
+  const cnbcEconomyResult = await cnbcEconomy;
+  const investingNewsResult = await investingNews;
+  const foolInvestingNewsResult = await foolInvestingNews;
 
-    const output = {
-        washingtonpost: washingtonpostResult,
-        usaTodaySports: usaTodaySportsResult,
-        cnnWorld: cnnWorldResult,
-        cnbcEconomy: cnbcEconomyResult,
-        investingNews: investingNewsResult,
-        foolInvestingNews: foolInvestingNewsResult,
-    };
+  const output = {
+    cryptonews: cryptonewsResult,
+    washingtonpost: washingtonpostResult,
+    usaTodaySports: usaTodaySportsResult,
+    cnnWorld: cnnWorldResult,
+    cnbcEconomy: cnbcEconomyResult,
+    investingNews: investingNewsResult,
+    foolInvestingNews: foolInvestingNewsResult,
+  };
 
-    fs.writeFile(
-        '/home/godzillanewz/public_html/parseroutput.json',
-        JSON.stringify(output),
-        (err) => {
-            if (err) throw err;
-            console.log('The parseroutput.json has been saved!');
-        },
-    );
+  fs.writeFile(
+    '/home/godzillanewz/public_html/parseroutput.json',
+    JSON.stringify(output),
+    (err) => {
+      if (err) throw err;
+      console.log('The parseroutput.json has been saved!');
+    },
+  );
 };
 
 writeOutput();
