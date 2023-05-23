e77e29d472e6de5a2c9f087b4aa0a50fd585c2c6
Vyacheslav Propastin <Propastin.V@outlook.com>
2023-05-23 13:11:14 +0000

    openai

--- newsSources/ig.js
+++ newsSources/ig.js
@@ -9,8 +9,16 @@ import cleanHTML from '../components/cleanHTML.js';
 import checkTitle from '../components/checkTitle.js';
 import addNewsToDB from '../components/addNewsToDB.js';
 import generateXML from '../components/generateXML.js';
+import { Low, JSONFile } from 'lowdb';
+import connectDatabase from '../connectDatabase.js';
+import {Configuration, OpenAIApi} from "openai";
 
 dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 new Promise((resolve, reject) => {
   getNewsFromSource(
@@ -93,20 +101,60 @@ new Promise((resolve, reject) => {
         a: 'unwrap',
       });
 
-      news.push({
-        title: title.replace(/\n/g, '').replace(/  +/g, '').replace(/ +$/, ''),
-        link: item,
-        pubDate: generateDate(),
-        /*
-        description: `<img src="${image.slice(0, image.indexOf('g?') + 1)}" /> ${description.replace(
-          /\n/g,
-          '',
-        )}<br><div>This post appeared first on ig.com</div>`,*/
-        description: `<img src="https:${image}" /> ${description.replace(
-          /\n/g,
-          '',
-        )}<br><div>This post appeared first on ig.com</div>`,
-      });
+      const gpt_prompt = "Write a well-structured and unique article. Use this link as a reference: "+item+".";
+
+      async function fetchCompletion(attempts = 1) {
+        try {
+          const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: gpt_prompt,
+            max_tokens: 2048,
+            temperature: 0,
+          });
+      
+          if (response.status === 200) {
+            console.log(response.data.choices[0].text);
+            const gpt_description = response.data.choices[0].text;
+            news.push({
+              title: title.replace(/\n/g, '').replace(/  +/g, '').replace(/ +$/, ''),
+              link: item,
+              pubDate: generateDate(),
+              image,
+              /*
+              description: `<img src="${image.slice(0, image.indexOf('g?') + 1)}" /> ${description.replace(
+                /\n/g,
+                '',
+              )}<br><div>This post appeared first on ig.com</div>`,*/
+              description: `<img src="https:${image}" /> ${gpt_description.replace(
+                /\n/g,
+                '',
+              )}<br><div>This post appeared first on ig.com</div>`,
+            });
+
+            
+          } else {
+            console.log("Error: Unexpected response from OpenAI API");
+            if (attempts < 10) {
+              // Asking again
+              await fetchCompletion(attempts + 1);
+            } else {
+              console.log("Reached maximum number of attempts.");
+            }
+          }
+        } catch (error) {
+          console.log("Error:", error.message);
+          if (attempts < 10) {
+            // Asking again
+            await fetchCompletion(attempts + 1);
+          } else {
+            console.log("Reached maximum number of attempts.");
+          }
+        }
+      }
+      
+      fetchCompletion();
+
+      console.log(item);
     }
 
     return news;
@@ -114,12 +162,63 @@ new Promise((resolve, reject) => {
   .then(async (news) => {
     await addNewsToDB(news, 'ig.json');
   })
-  .then(() => {
+  .then(async () => {
     const xml = baseXML(
       'https://edition.cnn.com/world',
       'World News from CNN',
       'Get the latest news from the CNN',
     );
+
+    // забрать из БД инфу
+    const db = await connectDatabase(`${pathToFile}`).then((path) => {
+      pathToDataBase = path;
+    });
+  
+    const adapter = new JSONFile(pathToDataBase);
+    const dataBase = new Low(adapter);
+    await dataBase.read();
+    const { item } = dataBase.data;
+    const link = item.map((item)=>{item.link});
+    link.map((item)=>{
+      async function fetchCompletion(attempts = 1) {
+        try {
+          const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: gpt_prompt,
+            max_tokens: 2048,
+            temperature: 0,
+          });
+      
+          if (response.status === 200) {
+            console.log(response.data.choices[0].text);
+            const gpt_description = response.data.choices[0].text;
+            gpt_description
+
+            
+          } else {
+            console.log("Error: Unexpected response from OpenAI API");
+            if (attempts < 10) {
+              // Asking again
+              await fetchCompletion(attempts + 1);
+            } else {
+              console.log("Reached maximum number of attempts.");
+            }
+          }
+        } catch (error) {
+          console.log("Error:", error.message);
+          if (attempts < 10) {
+            // Asking again
+            await fetchCompletion(attempts + 1);
+          } else {
+            console.log("Reached maximum number of attempts.");
+          }
+        }
+      }
+      
+      fetchCompletion();
+    })
+
+    // Описание из чатгпт
 
     generateXML(
       'ig.json',
